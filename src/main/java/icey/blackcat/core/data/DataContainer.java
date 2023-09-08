603c1ca01f56d8888c22a61d58fd654ddfa88171
little-Icey <psychofrai@gmail.com>
2023-09-08 17:49:17 +0800

    add structure DataContainer for search class or method node and their call relations in the graph

---
+++ src/main/java/icey/blackcat/core/data/DataContainer.java
@@ -0,0 +1,107 @@
+package icey.blackcat.core.data;
+
+import icey.blackcat.bean.Call;
+import icey.blackcat.bean.ClassReference;
+import icey.blackcat.bean.Has;
+import icey.blackcat.bean.MethodReference;
+import org.springframework.beans.factory.annotation.Autowired;
+import soot.SootClass;
+import soot.SootMethod;
+import soot.SootMethodRef;
+import soot.jimple.MulExpr;
+
+import java.util.*;
+
+public class DataContainer {
+
+    // 先不将信息持久化，只保存在内存中
+
+    private Map<String, ClassReference> savedClassRefs = Collections.synchronizedMap(new HashMap<>());
+
+    private Map<String, MethodReference> savedMethodRefs = Collections.synchronizedMap(new HashMap<>());
+
+    private Set<Call> savedCallNodes = Collections.synchronizedSet(new HashSet<>());
+    private Set<Has> savedHasNodes = Collections.synchronizedSet(new HashSet<>()); // 后续要删掉，不需要Has边
+
+    /**
+     * 将程序语义节点保存到内存
+     */
+    public <T> void store(T ref){
+        if(ref == null) return;
+
+        if(ref instanceof ClassReference){
+            ClassReference classRef = (ClassReference) ref;
+            savedClassRefs.put(classRef.getName(), classRef);
+        } else if (ref instanceof MethodReference) {
+            MethodReference methodRef = (MethodReference) ref;
+            savedMethodRefs.put(methodRef.getSubSignature(), methodRef);
+        } else if (ref instanceof Call) {
+            savedCallNodes.add((Call) ref);
+        }
+    }
+
+    /**
+     * 通过类名查找class节点
+     */
+    public ClassReference getClassRefByName(String name){
+        ClassReference ref = savedClassRefs.getOrDefault(name, null);
+        return ref;
+    }
+
+    /**
+     * 通过subsignature和所属类 获取指定method节点
+     * 注：函数签名带有类名，子签名没有类名
+     */
+    public MethodReference getMethodRefBySubSignature(String classname, String subSignature){
+        String signature = String.format("<%s: %s>", subSignature.replace("'", ""));
+        MethodReference ref = savedMethodRefs.getOrDefault(signature, null);
+        return ref;
+    }
+
+    /**
+     * 通过完整签名查找指定的method节点
+     */
+    public MethodReference getMethodRefBySignature(String signature){
+        MethodReference ref = savedMethodRefs.getOrDefault(signature, null);
+        return ref;
+    }
+
+    /**
+     * overloading
+     */
+    public MethodReference getMethodRefBySignature(SootMethodRef sootMethodRef){
+        SootClass cls = sootMethodRef.getDeclaringClass();
+        String subSignature = sootMethodRef.getSubSignature().toString();
+        MethodReference target = getMethodRefBySubSignature(cls.getName(), subSignature);
+        return target != null? target : getFirstMethodRefFromFatherNodes(cls, subSignature);
+    }
+
+    /**
+     * 广度优先搜素
+     */
+    public MethodReference getFirstMethodRefFromFatherNodes(SootClass cls, String subSignature){
+        MethodReference target = null;
+
+        if(cls.hasSuperclass()){
+            SootClass superCls = cls.getSuperclass();
+            target = getTargetMethodRef(superCls, subSignature);
+            if(target != null) return target;
+        }
+
+        if(cls.getInterfaceCount() > 0){
+            for(SootClass intFace : cls.getInterfaces()){
+                target = getTargetMethodRef(cls, subSignature);
+                if(target != null) return target;
+            }
+        }
+        return null;
+    }
+
+    private MethodReference getTargetMethodRef(SootClass cls, String subSignature){
+        MethodReference target = getMethodRefBySubSignature(cls.getName(), subSignature);
+        if(target == null){
+            target = getFirstMethodRefFromFatherNodes(cls, subSignature);
+        }
+        return target;
+    }
+}
